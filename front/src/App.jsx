678e37a13d36e93b071d5bd34ffa83abb94f807a
Alejandro Velardo <a.velardo.lucero@gmail.com>
2023-06-09 12:04:32 +0200

    Router created

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Route, Routes} from "react-router-dom"
+import Context from "./context/Context"
 
+
+import { Home } from './pages/Home'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const context = { }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <Context.Provider value={context}>
+      <div className='app'>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Context.Provider>
+
   )
 }
 
